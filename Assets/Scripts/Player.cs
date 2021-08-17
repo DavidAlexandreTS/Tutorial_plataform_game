f5ad5019880021fac416820425d1fef576530ad7
DavidAlexandreTS <dsa@ic.ufal.br>
2021-08-17 12:16:30 -0300

    Adicionando animações do pulo e movimentacoes,lembrar de ajeitar os comentarios bilingues

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,16 @@ public class Player : MonoBehaviour
 
     public bool isJumping;
     public bool doubleJump;
+
+    private Animator anime;
+
     private Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,13 +28,43 @@ public class Player : MonoBehaviour
         Jump();
     }
 
+    // Move the character
     void Move()
     {
         // Create moves on x
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        // Personagem indo para a direita
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            // Character is walking
+            anime.SetBool("walk", true);
+
+            // Personagem virado pra direita
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        // Personagem indo para a esquerda
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            // Character is walking
+            anime.SetBool("walk", true);
+
+            // Para virar o bonito pra esquerda
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        // Sempre que o personagem tiver parado Input.GetAxis("Horizontal") será 0
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            // Character is not walking
+            anime.SetBool("walk", false);
+        }
+        
     }
 
+    // Character jump rsrs
     void Jump()
     {
         if(Input.GetButtonDown("Jump"))
@@ -38,6 +73,7 @@ public class Player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anime.SetBool("walk", true);
             }
             else
             {
@@ -51,14 +87,17 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Metodo para checar se o personagem tocou no chao
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anime.SetBool("walk", false);
         }
     }
 
+    // Metodo para checar se o personagem saiu do chao \o/
     void OnCollisionExit2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 8)
