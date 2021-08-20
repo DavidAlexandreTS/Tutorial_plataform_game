7d65468968580306514daf15d2a7094127583928
DavidAlexandreTS <dsa@ic.ufal.br>
2021-08-20 08:57:19 -0300

    criando fase 2

---
+++ Assets/Scripts/FallingPlataform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlataform : MonoBehaviour
+{
+    public float FallingTime;
+    
+    private TargetJoint2D target;
+    private BoxCollider2D boxcool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<TargetJoint2D>();
+        boxcool = GetComponent<BoxCollider2D>();
+    }
+
+    // Metodo para checar se tocou no personagem
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //se tocou cai dps de 2secs
+        if(collision.gameObject.tag == "Player")
+        {
+            //nosso metodo vai ser chamado depois de fallingtimesegundos
+            Invoke("Falling", FallingTime);
+        }
+    }
+
+    //destroi se tocar em algo com a layer 9
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+         if(collider.gameObject.layer == 9)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Falling()
+    {
+        target.enabled = false;
+        boxcool.isTrigger = true;
+    }
+ 
+}
